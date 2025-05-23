5f4800bc5106c04fc2f045c1f96246d03465425c
Thushan Fernando <thushan@thushanfernando.com>
2025-05-24 08:45:20 +1000

    version banner  tweaks

--- internal/version/version.go
+++ internal/version/version.go
@@ -26,28 +26,28 @@ const (
 func PrintVersionInfo(extendedInfo bool, vlog *log.Logger) {
 	githubUri := theme.Hyperlink(GithubHomeUri, GithubHomeText)
 	latestUri := theme.Hyperlink(GithubLatestUri, Version)
-	padLatest := fmt.Sprintf("%*s", 1-len(Version), "")
+	padLatest := fmt.Sprintf("%*s", 2-len(Version), "")
 	padBuffer := fmt.Sprintf("%*s", 2, "")
 
 	var b strings.Builder
 
-	b.WriteString(theme.ColourSplash(`╔────────────────────────────────────────────────────────╗
-│                                      ⠀⠀⣀⣀⠀⠀⠀⠀⠀⣀⣀⠀⠀     │
-│                                      ⠀⢰⡏⢹⡆⠀⠀⠀⢰⡏⢹⡆⡀     │ 
-│   ██████╗ ██╗     ██╗      █████╗    ⠀⢸⡇⣸⡷⠟⠛⠻⢾⣇⣸⡇      │
-│  ██╔═══██╗██║     ██║     ██╔══██╗   ⢠⡾⠛⠉⠁⠀⠀⠀⠈⠉⠛⢷⡄     │
-│  ██║   ██║██║     ██║     ███████║   ⣿⠀⢀⣄⢀⣠⣤⣄⡀⣠⡀⠀⣿     │
-│  ██║   ██║██║     ██║     ██╔══██║   ⢻⣄⠘⠋⡞⠉⢤⠉⢳⠙⠃⢠⡿⡀    │
-│  ╚██████╔╝███████╗███████╗██║  ██║   ⣼⠃⠀⠀⠳⠤⠬⠤⠞⠀⠀⠘⣷     │
-│                                      ⢸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇     │` + "\n"))
+	b.WriteString(theme.ColourSplash(`╔─────────────────────────────────────────────────────╗
+│                                      ⠀⠀⣀⣀⠀⠀⠀⠀⠀⣀⣀⠀⠀  │
+│                                      ⠀⢰⡏⢹⡆⠀⠀⠀⢰⡏⢹⡆⡀  │ 
+│   ██████╗ ██╗     ██╗      █████╗    ⠀⢸⡇⣸⡷⠟⠛⠻⢾⣇⣸⡇   │
+│  ██╔═══██╗██║     ██║     ██╔══██╗   ⢠⡾⠛⠉⠁⠀⠀⠀⠈⠉⠛⢷⡄  │
+│  ██║   ██║██║     ██║     ███████║   ⣿⠀⢀⣄⢀⣠⣤⣄⡀⣠⡀⠀⣿  │
+│  ██║   ██║██║     ██║     ██╔══██║   ⢻⣄⠘⠋⡞⠉⢤⠉⢳⠙⠃⢠⡿⡀ │
+│  ╚██████╔╝███████╗███████╗██║  ██║   ⣼⠃⠀⠀⠳⠤⠬⠤⠞⠀⠀⠘⣷  │
+│                                      ⢸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇  │` + "\n"))
 
-	b.WriteString(theme.ColourSplash("│ "))
+	b.WriteString(theme.ColourSplash("│  "))
 	b.WriteString(theme.StyleUrl(githubUri))
 	b.WriteString(padLatest)
 	b.WriteString(theme.ColourVersion(latestUri))
 	b.WriteString(padBuffer)
-	b.WriteString(theme.ColourSplash(" ⢸⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿     │\n"))
-	b.WriteString(theme.ColourSplash("╚────────────────────────────────────────────────────────╝"))
+	b.WriteString(theme.ColourSplash(" ⢸⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿  │\n"))
+	b.WriteString(theme.ColourSplash("╚─────────────────────────────────────────────────────╝"))
 
 	if extendedInfo {
 		b.WriteString("\n")
