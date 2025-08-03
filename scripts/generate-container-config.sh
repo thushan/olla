1f43dab34345f0579d670deb45047016608cf3d7
Thushan Fernando <thushan@thushanfernando.com>
2025-08-03 11:12:52 +1000

    cleanup configuration, avoid maintaining docker.yaml manually.

---
+++ scripts/generate-container-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# creates a Docker configuration file from the base configuration.
+
+# Update endpoints to use host.docker.internal for local development
+sed 's|- url: "http://localhost:|- url: "http://host.docker.internal:|g; s|- url: "http://127\.0\.0\.1:|- url: "http://host.docker.internal:|g' config/config.yaml > config/docker.yaml
+
+# update the host: "localhost" to be 0.0.0.0 for Docker compatibility
+sed -i 's|host: "localhost"|host: "0.0.0.0"|g' config/docker.yaml
+
+# update the header  from '# Olla Configuration (default)' to '# Olla Configuration (docker)'
+sed -i 's|# Olla Configuration (default)|# Olla Configuration (docker)|g' config/docker.yaml
