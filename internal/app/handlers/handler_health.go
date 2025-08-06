a988c451ec5697780779516e03a038042237a0d1
Thushan Fernando <thushan@thushanfernando.com>
2025-08-06 18:35:08 +1000

    slightly better way to handle status

--- internal/app/handlers/handler_health.go
+++ internal/app/handlers/handler_health.go
@@ -1,18 +1,16 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/thushan/olla/internal/core/constants"
 )
 
-var response = map[string]string{"status": "healthy"}
+var responseJSON = []byte(`{"status":"healthy"}`)
 
 // healthHandler handles health check requests
 func (a *Application) healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set(constants.HeaderContentType, constants.ContentTypeJSON)
 	w.WriteHeader(http.StatusOK)
-
-	_ = json.NewEncoder(w).Encode(response)
+	_, _ = w.Write(responseJSON)
 }
