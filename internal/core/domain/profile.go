8573d0f0d700bb925685473c558ea6c8645254c8
Thushan Fernando <thushan@thushanfernando.com>
2026-02-19 21:58:45 +1100

    initial DMR

--- internal/core/domain/profile.go
+++ internal/core/domain/profile.go
@@ -8,6 +8,7 @@ const (
 	ProfileSGLang           = "sglang"
 	ProfileVLLM             = "vllm"
 	ProfileOpenAICompatible = "openai-compatible"
+	ProfileDockerMR         = "docker-model-runner"
 	ProfileAuto             = "auto"
 )
 
